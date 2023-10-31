95659a7564d54bad478b1aed5424b851870c1519
adams miracle <adamsmiracle0@gmail.com>
2023-10-31 16:36:28 +0000

    task 10

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,4 +1,4 @@
-#inclue "lists.h"
+#include "lists.h"
 
 /**
  * check_cycle - checks for a cycle in a linked list
@@ -8,14 +8,15 @@
 
 int check_cycle(listint_t *list)
 {
-	listint_t *fast = list, listint_t *slow = list;
+	listint_t *fast = list;
+	listint_t *slow = list;
 
 	while (fast && slow && fast->next)
 	{
 		slow = slow->next;
 		fast = fast->next->next;
-		if (flow == fast)
+		if (slow == fast)
 			return (1);
 	}
-	return (0)
+	return (0);
 }
