327eb2f5031b1dfff0e98c3dd7670b3f28f250bd
adams miracle <adamsmiracle0@gmail.com>
2023-11-03 22:36:49 +0000

    task 3

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+
+    args = sys.argv    
+    size = len(args) - 1
+
+    if size > 1:
+        print(f"{} arguments".format(size))
+        for i in range(size + 1):
+            print(f"{}: {}".format(i, argv[i]))
+
+    elif size == 0:
+        print(f"{} arguments".format(size))
+
+    else:
+        print(f"{} arguments".format(size))
+        print(f"{}: {}".format(size, argv[i]))
