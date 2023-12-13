871060739c31591c1759ea1aaf9ef3b50d84780e
adams miracle <adamsmiracle0@gmail.com>
2023-12-13 16:07:35 +0000

    task 0

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,37 @@
+
+#!/usr/bin/python3
+
+"""Defines a base model class"""
+
+import csv
+import json
+import turtle
+
+class base:
+    """Base model
+
+    This represents the 'base' class for the entire project
+
+    Private class attributes:
+        __nb_objects (int): number of instantiated objects
+    """
+
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """Instantiate a new base
+
+        Args:
+            id (int): identity of the new base
+
+        """
+
+        if id is not None:
+            self.id = id
+
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
+
+
+
