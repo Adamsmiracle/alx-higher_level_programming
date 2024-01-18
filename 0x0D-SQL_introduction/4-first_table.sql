c958558355445942e9189fd09135ffcd3966a615
adams miracle <adamsmiracle0@gmail.com>
2024-01-18 21:22:46 +0000

    task 4

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,5 +1,5 @@
 --- creates a table with attributes id and name
-CREATE TABLE IF NOT EXISTS `first_table` {
+CREATE TABLE IF NOT EXISTS `first_table` ()
     'id', INT
     'name', VARCHAR(256)
-}
+)
