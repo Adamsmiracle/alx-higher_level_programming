8b079dc793d2d97171a56c9bb2bc0c7c83eb7290
adams miracle <adamsmiracle0@gmail.com>
2024-01-19 14:55:34 +0000

    task 5

--- 0x0E-SQL_more_queries/5-unique_id.sql
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -1,3 +1,3 @@
---- script that creates the table unique_id on your MySQL server.
---- create a table
+-- creates the table unique_id on your MySQL server
+-- creates a table
 CREATE TABLE IF NOT EXISTS unique_id (id INT DEFAULT 1 UNIQUE, name VARCHAR(256));
