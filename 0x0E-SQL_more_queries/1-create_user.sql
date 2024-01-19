020bf1102ebe5fc8898902a08ea595fe752b7fb5
adams miracle <adamsmiracle0@gmail.com>
2024-01-19 09:05:49 +0000

    task 1

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,3 +1,3 @@
 --- Creates user and grants him all the privileges
 CREATE USER IF NOT EXISTS user_0d_1@localhost IDENTIFIED BY 'user_0d_1_pwd';
-GRANTS ALL PRIVILEGES ON " . " TO user_0d_1@localhost;
+GRANTS ALL PRIVILEGES ON * . * TO user_0d_1@localhost;
