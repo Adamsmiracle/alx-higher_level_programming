7cf0447844e9c62d5692f65ba47b4ea61ea2da3f
adams miracle <adamsmiracle0@gmail.com>
2024-01-19 09:22:43 +0000

    task 3

--- 0x0E-SQL_more_queries/3-force_name.sql
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -1,4 +1,3 @@
---- Creates table id_not_null on mysql server
-CREATE TABLE IF NOT EXISTS force_name (
-    id INT, name VARCHAR(256) NOT NULL
-);
+-- creates the table force_name on your MySQL server
+-- creates a table in a database
+CREATE TABLE IF NOT EXISTS force_name (id INT, name VARCHAR(256) NOT NULL);
