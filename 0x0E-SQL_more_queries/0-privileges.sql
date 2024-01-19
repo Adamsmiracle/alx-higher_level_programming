095d68b300a6002a5729b9dda1e1896853739c20
adams miracle <adamsmiracle0@gmail.com>
2024-01-19 08:56:23 +0000

    task 0

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,3 @@
---- Prints the privileges of users user_0d_1 and user_0d_2 on MYSQL SERVER
+--- Lists the privileges of the MYSQL users user_0d_1 and user_0d_2
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
