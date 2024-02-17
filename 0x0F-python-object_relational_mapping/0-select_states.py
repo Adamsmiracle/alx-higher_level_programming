bdf1e2e14af4289072dbabf5790eba69d97c1655
adams miracle <adamsmiracle0@gmail.com>
2024-02-17 15:32:56 +0000

    task 0

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -10,7 +10,7 @@ if __name__ == "__main__":
                          passwd=sys.argv[2], db=sys.argv[3], port=3306)
 
     cur = db.cursor()
-    cur.execute("SELECT * FROM states").order_by("state.id")
+    cur.execute("SELECT * FROM states")
 
     rows = cur.fetchall()
 
