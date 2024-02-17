ac09027517c05f1e800526976a785568fda162b1
adams miracle <adamsmiracle0@gmail.com>
2024-02-17 15:31:23 +0000

    task 0

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -5,7 +5,7 @@ import sys
 
 """ list all states from the database hbtn_0e_0_usa """
 
-if __name__ = "__main__":
+if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3], port=3306)
 
