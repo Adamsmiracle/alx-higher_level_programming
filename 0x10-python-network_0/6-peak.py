90cfeea0cad7a6329522faefe64f432bfac4e1e2
adams miracle <adamsmiracle0@gmail.com>
2024-03-05 14:05:50 +0000

    task 6

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+"""
+Finds the the peak element of the list
+"""
+def find_peakk(list_of_integers):
+    """ finds the peak element of the list"""
+    if not list_of_integers:
+        return None 
+    list_of_integers.sort()
+    return (list_of_integers[-1])
