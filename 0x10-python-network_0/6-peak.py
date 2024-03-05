c1794202eeae1c3232edfc1411429f378b743b53
adams miracle <adamsmiracle0@gmail.com>
2024-03-05 14:17:45 +0000

    task 6

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -3,9 +3,11 @@
 """
 Finds the the peak element of the list
 """
+
+
 def find_peak(list_of_integers):
     """ finds the peak element of the list"""
     if not list_of_integers:
-        return None 
+        return None
     list_of_integers.sort()
     return (list_of_integers[-1])
