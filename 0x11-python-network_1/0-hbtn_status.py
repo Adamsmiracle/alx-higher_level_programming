5921e8f4275bf74c55cbc258e3c0aea10148b416
adams miracle <adamsmiracle0@gmail.com>
2024-03-06 09:15:47 +0000

    task 0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,18 +1,13 @@
 #!/usr/bin/python3
-"""
-script that hat fetches https://alx-intranet.hbtn.io/status
-
-    You must use the package urllib
-"""
-
+"""Makes a fetch request to a url"""
 import urllib.request
 
 
 if __name__ == "__main__":
     url = 'https://alx-intranet.hbtn.io/status'
     with urllib.request.urlopen(url) as response:
-        response = response.read()
-        print("Response body:")
-        print(f"\t- type: {type(response)}")
-        print(f"\t- content: {response}")
-        print(f"\t- utf8 content: {response.decode('utf-8')}")
+        res = response.read()
+        print("Body response:")
+        print(f"\t- type: {type(res)}")
+        print(f"\t- content: {res}")
+        print(f"\t- utf8 content: {res.decode('utf-8')}")
