cbfa2351a3df3258781c3f3b3061f15bea9ad695
adams miracle <adamsmiracle0@gmail.com>
2024-03-06 12:12:10 +0000

    task 4

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -11,5 +11,5 @@ if __name__ == "__main__":
     url = 'https://alx-intranet.hbtn.io/status'
     res = requests.get(url)
     print("Body response: ")
-    print(f"\t- type: {res.text)}")
+    print(f"\t- type: {res.text}")
     print(f"\t- content: {res.text}")
