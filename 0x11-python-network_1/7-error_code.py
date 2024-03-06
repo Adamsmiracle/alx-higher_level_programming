70e71db7daa5e89d6feb20cd0cb2295de8a87414
adams miracle <adamsmiracle0@gmail.com>
2024-03-06 13:03:47 +0000

    task 7

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,24 +1,15 @@
-import requests
+#!/usr/bin/python3
+"""Prints the reponse if the Status Code 
+of the response is less than 400"""
 import sys
+import requests
 
-def display_response_body(url):
-    try:
-        response = requests.get(url)
-        # Check if response status code is >= 400
-        if response.status_code >= 400:
-            print(f"Error code: {response.status_code}")
-        else:
-            print(response.text)
-    except requests.exceptions.RequestException as e:
-        print(f"Error: {e}")
 
 if __name__ == "__main__":
-    # Check if URL is provided as an argument
-    if len(sys.argv) != 2:
-        print("Usage: python script.py <URL>")
-        sys.exit(1)
-
     url = sys.argv[1]
-
-    display_response_body(url)
-
+    res = requests.get(url)
+    try:
+        res.raise_for_status()
+        print(res.text)
+    except requests.exceptions.HTTPError as e:
+        print("Error code: {}".format(res.status_code))
