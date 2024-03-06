3eed3a758d34bccc8050c63e392b69fecaed7484
adams miracle <adamsmiracle0@gmail.com>
2024-03-06 09:27:26 +0000

    task 1

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
-"""
-accepts url and sends request to the url
-"""
+"""Makes a send request to the url from argv[1]"""
 
-import urllib.request
 import sys
+import urllib.request
+
 
 if __name__ == "__main__":
     url = sys.argv[1]
-    with urllib.request.urlopen(url) as response:
-        value = response.getheader("X-Request-id")
-        print(value)
+    with urllib.request.urlopen(url) as res:
+        print(res.getheader("X-Request-id"))
