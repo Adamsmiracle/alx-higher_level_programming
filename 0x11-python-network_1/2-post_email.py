cad8826106b1c294881775971487d9aa8eded6b0
adams miracle <adamsmiracle0@gmail.com>
2024-03-06 10:03:32 +0000

    task 2

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,2 +1,19 @@
 #!/usr/bin/python3
+"""
+ script that takes in a URL and an email, sends a POST 
+ request to the passed URL with the email as a parameter, 
+ and displays the body of the response (decoded in utf-8)
+"""
 
+import urllib.request
+import sys
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    email = sys.argv[2]
+    param = {"email": email}
+    data = urllib.parse.urlencode(param)
+    data = data.encode('ascii')
+    request = urllib.request.Request(url, data)
+    with urllib.request.urlopen(request) as res:
+        print(res.read().decode('utf-8'))
