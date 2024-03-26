4562abbf0e1aaba27f393de10bd016ec5c9b86ec
adams miracle <adamsmiracle0@gmail.com>
2024-03-26 22:20:08 +0000

    task 2

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,8 +1,5 @@
 #!/usr/bin/node
 const request = require('request');
-request.get((process.argv[2]), (res, error) => {
-  if (error) {
-    console.log('error: ', error);
-  }
-  console.log(`code: ${res.statusCode}`);
+request.get(process.argv[2]).on('response', function (response) {
+  console.log(`code: ${response.statusCode}`);
 });
