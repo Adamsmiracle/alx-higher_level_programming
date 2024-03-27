5220e41c3b86b111af8b4978b4d20b2957e5cc93
adams miracle <adamsmiracle0@gmail.com>
2024-03-27 01:14:11 +0000

    task 100

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,20 +1,21 @@
-const request = require('request');
+#!/usr/bin/node
 
+const req = require('request');
 const id = process.argv[2];
 const url = 'https://swapi-api.hbtn.io/api/films/';
-request.get(url + id, (error, res, body) => {
+req.get(url + id, function (error, res, body) {
   if (error) {
     console.log(error);
   }
   const data = JSON.parse(body);
   const dd = data.characters;
   for (const i of dd) {
-    request.get(i, (error, res, body) => {
+    req.get(i, function (error, res, body1) {
       if (error) {
         console.log(error);
       }
-      const data = JSON.parse(body);
-      console.log(data.name);
+      const data1 = JSON.parse(body1);
+      console.log(data1.name);
     });
   }
 });
