9aab13aed8a9b869fa6746517d08cd3a7dc85d8c
Adams Miracle <98392237+Adamsmiracle@users.noreply.github.com>
2024-05-23 22:25:19 +0000

    Delete 0x15-javascript-web_jquery directory

--- 0x15-javascript-web_jquery/0-script.js
+++
@@ -1,4 +0,0 @@
-function changeColor () {
-  document.querySelector('HEADER').style.color = '#FF0000';
-}
-changeColor();
--- 0x15-javascript-web_jquery/7-script.js
+++
@@ -1,9 +0,0 @@
-$(function () {
-  $.ajax({
-    type: 'GET',
-    url: 'https://swapi.co/api/people/5/?format=json',
-    success: function (data) {
-      $('DIV#character').text(data.name);
-    }
-  });
-});
